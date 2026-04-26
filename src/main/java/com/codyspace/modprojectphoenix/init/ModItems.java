38126b20e9efd46b6ff90aa7505f664f2612d5ab
alvaro <alvaro@alvaro>
2026-04-26 13:05:40 -0300

    Initial commit

---
+++ src/main/java/com/codyspace/modprojectphoenix/init/ModItems.java
@@ -0,0 +1,13 @@
+package com.codyspace.modprojectphoenix.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.Item;
+
+public class ModItems {
+
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	//public static final Item PHONE = new ItemBase("phone");
+}
